efd8b9facd0e025c124dd076dd486122c5e02e39
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2016-08-19 14:46:46 +0300

    update tests

--- DividendFlowCalculatorTest/DividendFlowCalculatorTest.cs
+++ DividendFlowCalculatorTest/DividendFlowCalculatorTest.cs
@@ -31,7 +31,7 @@ namespace GivenAData
 	{
 		private Matrix<double> shareholdings, outside, remainder, dividends;
 		private DividendData data;
-		Tuple<Matrix<double>, int> dynamicDividend;
+		private Matrix<double> dynamicDividend;
 		private Matrix<double> ownership;
 		private Matrix<double> penultimateExit;
 
@@ -70,7 +70,7 @@ namespace GivenAData
 				{ 64.0836644861, 1.1746496438, 0 }
 			});
 
-			dynamicDividend = System.Tuple.Create (Matrix<double>.Build.DenseOfArray(new double[,] {
+			dynamicDividend = Matrix<double>.Build.DenseOfArray(new double[,] {
 				{ 1000, 0, 24, 1.8, 1.008, 0.1188, 0.045576, 0.006804, 0.002128032, 0.0003678048, 0.0001016245, 1.92782592E-05, 4.930279488E-06, 9.926110656E-07 },
 				{ 0, 0, 18, 0, 0.756, 0.0324, 0.031752, 0.0027216, 0.001391904, 0.0001714608, 6.3358848E-05, 9.7067808E-06, 2.969701056E-06, 5.2173072E-07 },
 				{ 0, 120, 0, 5.04, 0.216, 0.21168, 0.018144, 0.00927936, 0.001143072, 0.0004223923, 6.4711872E-05, 0.000019798, 3.4782048E-06, 0.000000948 },
@@ -80,34 +80,34 @@ namespace GivenAData
 				{ 0, 770, 770, 788.48, 789.866, 790.64216, 790.733636, 790.76872952, 790.7739686, 790.7756071846, 790.7758903943, 790.7759686452, 790.7759834895, 790.7759872858 },
 				{ 0, 0, 0, 10.26, 10.26, 10.69092, 10.709388, 10.72748664, 10.729037952, 10.7298313373, 10.7299290699, 10.7299651845, 10.7299707173, 10.7299724101 },
 				{ 0, 0, 62.4, 62.4, 65.0208, 65.13312, 65.2431936, 65.25262848, 65.2574537472, 65.2580481446, 65.2582677886, 65.2583014388, 65.2583117338, 65.2583135425 }
-			}), 14);
+			});
 
 			data = new DividendFlowCalculator.DividendData ();
 			data.shareholdings = shareholdings;
 			data.outside = outside;
 			data.remainder = remainder;
 			data.dividends = dividends;
+			data.companies = new List<string> ();
 		}
 
 		[Test]
 		public void CorrectDynamicDidivend ()
 		{
-			var dynamicDividendResult = data.dynamicDividend ();
-			Assert.That (dynamicDividendResult.Item1, Is.EqualTo (dynamicDividend.Item1).Using (new MatrixWithinComparer ()));
-			Assert.AreEqual (dynamicDividendResult.Item2, dynamicDividend.Item2);
+			var dynamicDividendResult = data.dynamicDividend ().Item3;
+			Assert.That (dynamicDividendResult, Is.EqualTo (dynamicDividend).Using (new MatrixWithinComparer ()));
 		}
 
 		[Test]
 		public void CorrectOwnershipTable ()
 		{
-			var ownershipResult = data.ownership ();
+			var ownershipResult = data.ownership ().Item3;
 			Assert.That (ownershipResult, Is.EqualTo (ownership).Using (new MatrixWithinComparer ()));
 		}
 
 		[Test]
 		public void CorrectPenultimateExit ()
 		{
-			var penultimateExitResult = data.penultimateExit ();
+			var penultimateExitResult = data.penultimateExit ().Item3;
 			Assert.That (penultimateExitResult, Is.EqualTo (penultimateExit).Using (new MatrixWithinComparer ()));
 		}
 	}
